c64700d687cb00effb9c61c34d86b16e4e966a71
sidv1993 <sidv1993@gmail.com>
2017-04-29 17:36:06 -0500

    Updated Code

--- Helper files/DatasetCrawler/TagCleaner.py
+++ Helper files/DatasetCrawler/TagCleaner.py
@@ -16,11 +16,11 @@ def doStemming(w):
 
 def tagCleaner():
     emptyTag = '$$NOTAG$$'
-    base_directory = 'C:/Users/007ri/PycharmProjects/untitled/IR Proj'
+    base_directory = '/Users/sidverma/Desktop/'
     os.chdir(base_directory)
     #dirs = os.listdir(base_directory)
     globalTagsList = []
-    for fn in ['Final_Playlist_Users_0_1500.csv', 'Final_Playlist_Users_1500_3000.csv']:
+    for fn in ['Final0-1500.csv', 'Final1500-3000.csv']:
         df = pd.read_csv(fn)
         # print df
         tagsCol = df['Tags']
@@ -60,6 +60,57 @@ def tagCleaner():
         #print df.Tags.value_counts()
     return collections.Counter(globalTagsList)
 
+
+def fooo(inTopTags):
+    count = 0
+    for fn in ['Final0-1500.csv', 'Final1500-3000.csv']:
+        df = pd.read_csv(fn)
+        # print df
+        tagsCol = df['Tags']
+        index = 0
+        for i in tagsCol:
+            if i == '[]':
+                #df.set_value(index, 'Tags', emptyTag)
+                index += 1
+                continue
+            if not isinstance(i, str):
+                #df.set_value(index, 'Tags', emptyTag)
+                index += 1
+                continue
+            i = i.replace('[', '')
+            i = i.replace(']', '')
+            i = i.replace('u\'', '')
+            i = i.replace('\'', '')
+            i = i.replace(' ', '#')
+            i = i.replace(',#', ' ')
+            if not len(i):
+                #df.set_value(index, 'Tags', emptyTag)
+                index += 1
+                continue
+            iList = i.split()
+            isPresent = False
+            for t in set(iList):
+                t = t.lower()
+                t = t.replace('#', ' ')
+                tSubList = t.split()
+                tSortedList = []
+                for w in tSubList:
+                    tSortedList.append(doStemming(w))
+                tSortedList.sort()
+                newTag = " ".join(tSortedList)
+                print newTag
+                #globalTagsList.append(newTag)
+                if newTag in inTopTags:
+                    isPresent = True
+                    break
+            print 'Processing..', index
+            if isPresent:
+                count+=1
+            index += 1
+        #df.to_csv('Final_Playlist_Users_0_1500_NEW.csv')
+        #print df.Tags.value_counts()
+    return count
+
 freqDict = tagCleaner()
 '''
 for i in freqDict:
@@ -67,17 +118,26 @@ for i in freqDict:
 '''
 print len(freqDict)
 lists = sorted(freqDict.items(), key=operator.itemgetter(1), reverse = True)
+i = 0
+for tup in lists:
+    i += 1
+    if tup[1] < 5000:
+        break
+lists = lists[:i]
+print 'Number of Unique Tags: ', i
+print 'rows with tags :: ', str(fooo(lists))
 #lists = sorted(freqDict.items()) # sorted by key, return a list of tuples
 
-f = open('frew', 'w')
-for tu in lists :
-    f.write(str(tu)+'\n')
-print 'done1'
+# f = open('frew', 'w')
+# for tu in lists :
+#     f.write(str(tu)+'\n')
+# print 'done1'
 
-xTicks, y = zip(*lists[:100]) # unpack a list of pairs into two tuples
-plt.figure(figsize = (90,50))
-x = xrange(len(freqDict))
+xTicks, y = zip(*lists) # unpack a list of pairs into two tuples
+#plt.figure(figsize = (90,30))
+x = xrange(len(lists))
 plt.xticks(x, xTicks, rotation=90)
-plt.plot(x,y)
+# plt.plot(x,y)
+plt.bar(x, y, color='b')
 plt.show()
 print 'done'
