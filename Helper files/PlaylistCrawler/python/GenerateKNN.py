d96ac996b9adbad069c27835d524fb1f6609dd8f
sidv1993 <sidv1993@gmail.com>
2017-04-30 13:25:11 -0500

    KNN File

--- Helper files/PlaylistCrawler/python/GenerateKNN.py
+++ Helper files/PlaylistCrawler/python/GenerateKNN.py
@@ -2,6 +2,10 @@
 import pandas as pd
 import math
 import time
+import os
+
+basedir = '/Users/sidverma/Desktop/'
+os.chdir(basedir)
 
 def similarity_func(playlist1, playlist2):
     set2 = set(playlist2)
@@ -10,7 +14,7 @@ def similarity_func(playlist1, playlist2):
 split_ratio=0.8
 start=time.time()
 
-frame = pd.read_csv("../data/Final_playlist_new.csv")
+frame = pd.read_csv("Final.csv")
 frame.drop(["Playtime", "Album", "Match" , "Listeners", "Playcount", "Duration", "Tags"], axis=1, inplace="True")
 grouped=frame.groupby(by="Users")
 groups=grouped.groups
@@ -25,12 +29,16 @@ similarities=[]
 
 len_user_list=len(user_list)
 for i, user in enumerate(user_list):
+    print "Calculating 300NN for User", i
     similarity=[]
-    end_index=int(split_ratio*len(song_list[i]))
+    end_index=int(math.ceil(split_ratio*len(song_list[i])))
     curr_user_playlist=song_list[i][:end_index]
+    print "User details: ", user, len(curr_user_playlist)
     for j in range(0,i):
+        print "Parsing Row: ", j
         similarity.append((user_list[j],similarity_func(curr_user_playlist, song_list[j])))
     for j in range(i+1, len_user_list):
+        print "Parsing Row: ", j
         similarity.append((user_list[j], similarity_func(curr_user_playlist, song_list[j])))
     similarities.append(similarity)
 
@@ -38,7 +46,7 @@ for i, user in enumerate(user_list):
 for i in  range(len(similarities)):
     #similarities[i].sort(key=itemgetter(2), reverse="True")
     similarities[i].sort(key = lambda x: float(x[1]), reverse = True)
-    similarities[i]=similarities[i][:3]
+    similarities[i]=similarities[i][:300]
 
 similar_neighbors=[]
 similarity_scores=[]
@@ -51,7 +59,7 @@ for similarity in similarities:
 df = pd.DataFrame(
     data={"Users": user_list, "kNN": similar_neighbors, "Similarities": similarity_scores },
     columns=["Users", "kNN", "Similarities"])
-df.to_csv("../data/knnSimilarity.csv", sep=',')
+df.to_csv("knnSimilarity.csv", sep=',')
 
 
 
