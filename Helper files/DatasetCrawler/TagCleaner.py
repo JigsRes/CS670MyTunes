5a616096f8adad70e6d1bdb314ede7d6071a154d
risyadav <risyadav@tamu.edu>
2017-04-29 23:07:48 -0500

    Rewrite of Tag Cleaner
    Rewrite of Tag Cleaner

--- Helper files/DatasetCrawler/TagCleaner.py
+++ Helper files/DatasetCrawler/TagCleaner.py
@@ -47,12 +47,31 @@ def tagCleaner():
     emptyTag = '$$NOTAG$$'
     base_directory = '/Users/sidverma/Desktop/'
     os.chdir(base_directory)
-    #dirs = os.listdir(base_directory)
-    df1 = pd.read_csv('Final0-1500.csv')
+    #merge two files
+    df = pd.read_csv('Final0-1500.csv')
     df2 = pd.read_csv('Final1500-3000.csv')
-    df1 = df1.append(df2, axis = 0)
-    for fn in ['Final0-1500.csv', 'Final1500-3000.csv']:
-        
+    df = df.append(df2, axis = 0)
+    #start reading Tagscols
+    tagsCol = df['Tags']
+    index = 0
+    for i in tagsCol:
+        '''
+        if index == 20:
+            print "Exitting!"
+            return
+        print "Initial Tag: ", i
+        '''
+        if i == '[]':
+            df.set_value(index, 'Tags', emptyTag)
+            index += 1
+            continue
+        if not isinstance(i, str):
+            df.set_value(index, 'Tags', emptyTag)
+            index += 1
+            continue
+        newTag = GetNewTag(i)
+        df.set_value(index, 'Tags', newTag)
+        index += 1
     df.to_csv('Final0-2999.csv')
     return
 
@@ -61,81 +80,21 @@ def getTagFreqDict():
     base_directory = '/Users/sidverma/Desktop/'
     os.chdir(base_directory)
     globalTags = []
-    for fn in ['Final0-1500.csv', 'Final1500-3000.csv']:
-        df = pd.read_csv(fn)
-        tagsCol = df['Tags']
-        index = 0
-        for i in tagsCol:
-            if index == 20:
-                print "Exitting!"
-                return
-            print "Initial Tag: ", i
-            if i == '[]':
-                df.set_value(index, 'Tags', emptyTag)
-                index += 1
-                continue
-            if not isinstance(i, str):
-                df.set_value(index, 'Tags', emptyTag)
-                index += 1
-                continue
-            newTag = GetNewTag(i)
-            df.set_value(index, 'Tags', newTag)
-            index += 1
+    df = pd.read_csv('Final0-2999.csv')
+    tagsCol = df['Tags']
+    index = 0
+    emptyTag = '$$NOTAG$$'
+    for i in tagsCol:
+        iList = i.split()
+        for t in iList:
+            globalTags.append(t)
     return collections.Counter(globalTagsList)
 
-
-# def fooo(inTopTags):
-#     count = 0
-#     for fn in ['Final0-1500.csv', 'Final1500-3000.csv']:
-#         df = pd.read_csv(fn)
-#         # print df
-#         tagsCol = df['Tags']
-#         index = 0
-#         for i in tagsCol:
-#             if i == '[]':
-#                 #df.set_value(index, 'Tags', emptyTag)
-#                 index += 1
-#                 continue
-#             if not isinstance(i, str):
-#                 #df.set_value(index, 'Tags', emptyTag)
-#                 index += 1
-#                 continue
-#             i = i.replace('[', '')
-#             i = i.replace(']', '')
-#             i = i.replace('u\'', '')
-#             i = i.replace('\'', '')
-#             i = i.replace(' ', '#')
-#             i = i.replace(',#', ' ')
-#             if not len(i):
-#                 #df.set_value(index, 'Tags', emptyTag)
-#                 index += 1
-#                 continue
-#             iList = i.split()
-#             isPresent = False
-#             for t in set(iList):
-#                 t = t.lower()
-#                 t = t.replace('#', ' ')
-#                 tSubList = t.split()
-#                 tSortedList = []
-#                 for w in tSubList:
-#                     tSortedList.append(doStemming(w))
-#                 tSortedList.sort()
-#                 newTag = " ".join(tSortedList)
-#                 #globalTagsList.append(newTag)
-#                 if newTag in inTopTags:
-#                     isPresent = True
-#                     break
-#             print 'Processing..', index
-#             if isPresent:
-#                 count+=1
-#             index += 1
-#         #df.to_csv('Final_Playlist_Users_0_1500_NEW.csv')
-#         #print df.Tags.value_counts()
-#     return count
-
 def Retagger():
-    freqDict = tagCleaner()
+    tagCleaner()
+    freqDict = getTagFreqDict()
     print len(freqDict)
+    '''
     lists = sorted(freqDict.items(), key=operator.itemgetter(1), reverse = True)
     i = 0
     pop_tags = []
@@ -149,6 +108,7 @@ def Retagger():
         f.write(str(tu[0])+'\n')
     print 'done'
     f.close()
+    '''
 
 Retagger()
 
